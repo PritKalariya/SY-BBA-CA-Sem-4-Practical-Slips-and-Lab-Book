03ceefe842036330c5644ad96ce69a094cbcf80d
Prit Kalariya <kalariyaprit2000@gmail.com>
2021-03-15 22:40:57 +0530

    A3 A1

---
+++ CPP/Assignment 3/A1.cpp
@@ -0,0 +1,54 @@
+/*
+Write a C++ program to create a class Student with data members Roll_No, Student_Name, Class. Write member functions to accept and display Student information also display count of students. (Use Static data member and Static member function)
+*/
+
+#include<iostream>
+using namespace std;
+
+class Student {
+    static int count;
+    int roll_no;
+    char std_name[10], std_class[20];
+
+    public:
+        void add_student() {
+            cout << "Enter student roll no: ";
+            cin >> roll_no;
+            cout << "Enter student name: ";
+            cin >> std_name;
+            cout << "Enter student class: ";
+            cin >> std_class;
+            cout << endl;
+            count++;
+        }
+
+        void display_student() {
+            cout << "Student roll no: " << roll_no << endl;
+            cout << "Student name: " << std_name << endl;
+            cout << "Student class: " << std_class << endl;
+            cout << endl;
+        }
+};
+
+int Student :: count;
+
+int main() {
+    Student s1[10];
+    int n;
+
+    cout << "Enter the number of students: ";
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        s1[i].add_student();
+    }
+
+    cout << endl;
+
+    cout << "Student details are: " << endl;
+    for(int i=0; i<n; i++) {
+        s1[i].display_student();
+    }
+
+    return 0;
+}
